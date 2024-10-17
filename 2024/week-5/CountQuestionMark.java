44c865471ba9b420b2428dd81e17430e573e9d6d
Yifan <fengshun124@outlook.com>
2024-10-17 14:15:14 +0800

    update week-5 Lab implementation

---
+++ 2024/week-5/CountQuestionMark.java
@@ -0,0 +1,22 @@
+/*
+Lab #5.2 Count the occurrences of a question mark in a string
+ */
+public class CountQuestionMark {
+    public static void main(String[] args) {
+        System.out.println(countQuestionMark("?????"));
+        System.out.println(countQuestionMark("Hello, how are you?"));
+        System.out.println(countQuestionMark("CPT105"));
+        System.out.println(countQuestionMark(""));
+    }
+
+    public static int countQuestionMark(String str) {
+        // count the number of question marks in the string
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '?') {
+                count++;
+            }
+        }
+        return count;
+    }
+}
